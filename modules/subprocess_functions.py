b888861d4a34827aaa05a9e1892398b07e00b485
Víctor Sanz <116311046+v-sanz@users.noreply.github.com>
2022-10-27 18:19:04 +0200

    Update subprocess_functions.py

--- modules/subprocess_functions.py
+++ modules/subprocess_functions.py
@@ -1,7 +1,7 @@
 import subprocess
 
 def align(muscle_input, muscle_output):
-    subprocess.call(['muscle', '-in', muscle_input, '-out', muscle_output])
+    subprocess.call(['muscle', '-super5', muscle_input, '-output', muscle_output])
 
 def weblogo3(weblogo_format, muscle_output, weblogo_output):
     subprocess.call(['weblogo', '-D', 'fasta','-F', weblogo_format, '-A', 'protein', '-s', 'large', '-f', str(muscle_output),  '-o', str(weblogo_output)])
