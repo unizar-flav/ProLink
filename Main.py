cb4b1af43cacfab5c4a3bdd7fa7b3d89b183f132
Víctor Sanz <116311046+v-sanz@users.noreply.github.com>
2022-11-08 10:07:41 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -98,44 +98,44 @@ def pro_link(query_proteins, hitlist_range, blast_database, smart_blast_, max_lo
     my_sequences = obtain_fasta_file(query_proteins)
     for seq_n, my_seq_record in enumerate(my_sequences):
 
-    my_sequence_index = seq_n + 1
-    
-    os.mkdir("./outputs/protein_" + str(my_sequence_index))
-    with open('./outputs/protein_' + str(my_sequence_index) + '/report.txt', 'w') as f:
-        f.write('Protein_'+str(my_sequence_index)+" report:")
-        f.writelines("Protein_id: " + str(my_seq_record.id))
-    blast_filename = "./outputs/"+"protein_"+str(my_sequence_index)+"/blast_results.xml"
-    found_sequences_fastafile= "./outputs/"+"protein_"+str(my_sequence_index) + "/found_sequences.fasta"
+        my_sequence_index = seq_n + 1
 
-    if smart_blast_:
-        s_blast(my_sequence_index, blast_database, hitlist_range, my_seq_record, blast_filename, found_sequences_fastafile, remove_gaps, expected_min_identity, min_low_identity_seqs, max_low_identity_seqs, additional_hits)
-    else:
-        blast(hitlist_range, blast_database, blast_filename,  my_seq_record)
-        parse(my_sequence_index, hitlist_range, blast_filename, remove_gaps, expected_min_identity)
+        os.mkdir("./outputs/protein_" + str(my_sequence_index))
+        with open('./outputs/protein_' + str(my_sequence_index) + '/report.txt', 'w') as f:
+            f.write('Protein_'+str(my_sequence_index)+" report:")
+            f.writelines("Protein_id: " + str(my_seq_record.id))
+        blast_filename = "./outputs/"+"protein_"+str(my_sequence_index)+"/blast_results.xml"
+        found_sequences_fastafile= "./outputs/"+"protein_"+str(my_sequence_index) + "/found_sequences.fasta"
 
-    cluster_results_fastafile = "./outputs/"+"protein_"+str(my_sequence_index) + "/cluster_results_evaluation_" + str(similarity) + ".fasta"
-    cluster_results_file = "./outputs/"+"protein_"+str(my_sequence_index) + "/cluster_results_"+ str(similarity)
-    cluster_evaluation_file = "./outputs/"+"protein_"+str(my_sequence_index) +  "/cluster_results_evaluation_" + str(similarity)
-
-    if cluster_seqs:
-        if smart_clustering_:
-            s_cluster(found_sequences_fastafile, my_seq_record, similarity, min_number_of_clusters_to_cluster_again, max_number_of_clusters_to_cluster_again, cluster_results_file, cluster_evaluation_file, cluster_results_fastafile)
+        if smart_blast_:
+            s_blast(my_sequence_index, blast_database, hitlist_range, my_seq_record, blast_filename, found_sequences_fastafile, remove_gaps, expected_min_identity, min_low_identity_seqs, max_low_identity_seqs, additional_hits)
         else:
-            cluster(found_sequences_fastafile, my_seq_record, similarity, cluster_results_file, cluster_evaluation_file, cluster_results_fastafile)
-    
+            blast(hitlist_range, blast_database, blast_filename,  my_seq_record)
+            parse(my_sequence_index, hitlist_range, blast_filename, remove_gaps, expected_min_identity)
 
-    if align_seqs:
-        muscle_output= "./outputs/"+"protein_"+str(my_sequence_index) + "/cluster_results_evaluation_"  "aligned" ".fasta"
-        align(cluster_results_fastafile, muscle_output)
-        if generate_logo:
-            weblogo_output = "./outputs/"+"protein_"+str(my_sequence_index) +'/logo'+'.'+str(weblogo_format)
-            weblogo3(weblogo_format, muscle_output, weblogo_output)
-        if generate_tree:
-            mega_config_input = "./ProLink/modules/mega_configs/"+tree_type+"_"+bootstrap_replications+".mao"
-            mega_output = "./outputs/"+"protein_"+str(my_sequence_index) +"/"+tree_type +"_"+bootstrap_replications+"_tree" 
-            tree(mega_config_input, muscle_output, mega_output)
+        cluster_results_fastafile = "./outputs/"+"protein_"+str(my_sequence_index) + "/cluster_results_evaluation_" + str(similarity) + ".fasta"
+        cluster_results_file = "./outputs/"+"protein_"+str(my_sequence_index) + "/cluster_results_"+ str(similarity)
+        cluster_evaluation_file = "./outputs/"+"protein_"+str(my_sequence_index) +  "/cluster_results_evaluation_" + str(similarity)
 
-    else:
-        print("Process finished (no clustering)")
+        if cluster_seqs:
+            if smart_clustering_:
+                s_cluster(found_sequences_fastafile, my_seq_record, similarity, min_number_of_clusters_to_cluster_again, max_number_of_clusters_to_cluster_again, cluster_results_file, cluster_evaluation_file, cluster_results_fastafile)
+            else:
+                cluster(found_sequences_fastafile, my_seq_record, similarity, cluster_results_file, cluster_evaluation_file, cluster_results_fastafile)
+
+
+        if align_seqs:
+            muscle_output= "./outputs/"+"protein_"+str(my_sequence_index) + "/cluster_results_evaluation_"  "aligned" ".fasta"
+            align(cluster_results_fastafile, muscle_output)
+            if generate_logo:
+                weblogo_output = "./outputs/"+"protein_"+str(my_sequence_index) +'/logo'+'.'+str(weblogo_format)
+                weblogo3(weblogo_format, muscle_output, weblogo_output)
+            if generate_tree:
+                mega_config_input = "./ProLink/modules/mega_configs/"+tree_type+"_"+bootstrap_replications+".mao"
+                mega_output = "./outputs/"+"protein_"+str(my_sequence_index) +"/"+tree_type +"_"+bootstrap_replications+"_tree" 
+                tree(mega_config_input, muscle_output, mega_output)
+
+        else:
+            print("Process finished (no clustering)")
 
 pro_link(query_proteins, hitlist_range, blast_database, smart_blast_, max_low_identity_seqs, min_low_identity_seqs, expected_min_identity, additional_hits, remove_gaps, cluster_seqs, similarity, smart_clustering_, min_number_of_clusters_to_cluster_again, max_number_of_clusters_to_cluster_again, align_seqs, generate_logo, weblogo_format, generate_tree, tree_type, bootstrap_replications)
