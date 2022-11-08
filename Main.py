5be5d01936ab730405c939610a14ec4acbb94e32
Víctor Sanz <116311046+v-sanz@users.noreply.github.com>
2022-11-08 15:58:42 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -139,4 +139,4 @@ def pro_link(query_proteins, hitlist_range, blast_database, smart_blast_, max_lo
         else:
             print("Process finished (no clustering)")
 
-pro_link(query_proteins, hitlist_range, blast_database, smart_blast_, max_low_identity_seqs, min_low_identity_seqs, expected_min_identity, additional_hits, remove_gaps, cluster_seqs, similarity, smart_clustering_, min_number_of_clusters_to_cluster_again, max_number_of_clusters_to_cluster_again, align_seqs, generate_logo, weblogo_format, generate_tree, tree_type, bootstrap_replications)
+
