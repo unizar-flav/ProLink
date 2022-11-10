4975182752f5d85bcc022429b937b6762d959d62
Víctor Sanz <116311046+v-sanz@users.noreply.github.com>
2022-11-10 16:27:49 +0100

    Update blast.py

--- modules/blast.py
+++ modules/blast.py
@@ -57,7 +57,7 @@ def s_parse(my_sequence_index, hitlist_range, blast_filename, remove_gaps, expec
     sequence_index= 0
     for alignment in records.alignments:
         hsp = alignment.hsps[0]
-        if sequence_index < 12000:
+        if sequence_index < 11000:
           if low_identity_seqs < max_low_identity_seqs:
               rec_f = SeqRecord(
                           Seq(hsp.sbjct,
@@ -97,7 +97,7 @@ def s_blast(my_sequence_index, blast_database, hitlist_range, my_seq_record, bla
         print("Blasting again with " + str(hitlist_range) + " hits")
         blast(hitlist_range, blast_database, blast_filename,  my_seq_record)
         low_identity_seqs = 0
-        while s_parse(my_sequence_index, hitlist_range, blast_filename, remove_gaps, expected_min_identity, low_identity_seqs, max_low_identity_seqs, outputs_dir) < min_low_identity_seqs and hitlist_range < 12000:
+        while s_parse(my_sequence_index, hitlist_range, blast_filename, remove_gaps, expected_min_identity, low_identity_seqs, max_low_identity_seqs, outputs_dir) < min_low_identity_seqs and hitlist_range <= 11000:
             print()
             print("The number of low identity sequences is below the desired value")
             os.remove(blast_filename)
