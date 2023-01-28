a3c5455eb96f312e5c7f1253af4657359fdd4b9c
Víctor Sanz <116311046+v-sanz@users.noreply.github.com>
2023-01-28 18:19:54 +0100

    Update subprocess_functions.py

--- modules/subprocess_functions.py
+++ modules/subprocess_functions.py
@@ -4,7 +4,7 @@ def align(muscle_input, muscle_output):
     subprocess.call(['muscle', '-super5', muscle_input, '-output', muscle_output])
 
 def weblogo3(weblogo_format, muscle_output, weblogo_output):
-    subprocess.call(['weblogo', '-D', 'fasta','-F', weblogo_format, '-A', 'protein', '-s', 'large', '-f', str(muscle_output),  '-o', str(weblogo_output)])
+    subprocess.call(['weblogo', '-D', 'fasta','-F', weblogo_format, '-A', 'protein', '-s', 'large', '-n', '80', '-f', str(muscle_output),  '-o', str(weblogo_output)])
 
 def tree(mega_config_input, muscle_output, mega_output):
     subprocess.call(['megacc', '-a', mega_config_input, '-d', muscle_output, '-o', mega_output])
