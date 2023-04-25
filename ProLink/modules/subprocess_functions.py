7cefa97a569f6b5783ecbf13367d91d52364f3ca
Víctor Sanz <116311046+v-sanz@users.noreply.github.com>
2023-04-25 14:07:37 +0200

    Update subprocess_functions.py

--- ProLink/modules/subprocess_functions.py
+++ ProLink/modules/subprocess_functions.py
@@ -8,5 +8,6 @@ def align(muscle_input, muscle_output):
 def weblogo3(weblogo_format, muscle_output, weblogo_output):
     subprocess.call(['weblogo', '-D', 'fasta','-F', weblogo_format, '-A', 'protein', '-s', 'large', '-n', '80', '-f', str(muscle_output),  '-o', str(weblogo_output)])
 
-def tree(mega_config_input, muscle_output, mega_output):
+def tree(tree_type, bootstrap_replications, muscle_output, mega_output):
+    mega_config_input = str(tree_type + '_' + bootstrap_replications + '.mao')
     subprocess.call(['megacc', '-a', mega_config_input, '-d', muscle_output, '-o', mega_output])
