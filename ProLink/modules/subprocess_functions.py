da1ebdf6b13d660be408e12197f24f303f4d999a
Víctor Sanz <116311046+v-sanz@users.noreply.github.com>
2023-04-25 20:19:45 +0200

    Update subprocess_functions.py

--- ProLink/modules/subprocess_functions.py
+++ ProLink/modules/subprocess_functions.py
@@ -13,4 +13,5 @@ def weblogo3(weblogo_format, muscle_output, weblogo_output):
 
 def tree(tree_type, bootstrap_replications, muscle_output, mega_output):
     mega_config_input = str(tree_type + '_' + bootstrap_replications + '.mao')
+    print(mega_config_input)
     subprocess.call(['megacc', '-a', mega_config_input, '-d', muscle_output, '-o', mega_output])
