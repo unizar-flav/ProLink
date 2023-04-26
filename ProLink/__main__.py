6d6c3af1c1c7c9ba0a169917925ecc79aa154475
boneta <boneta@unizar.es>
2023-04-26 14:16:41 +0200

    Add options at CLI

--- ProLink/__main__.py
+++ ProLink/__main__.py
@@ -11,10 +11,20 @@ def main():
         description='Excecute multiple proteomic analysis tools automatically')
     parser.add_argument('UNIPROT_ID', type=str, nargs='+',
         help='UniProt code of the protein(s) to query')
+    parser.add_argument('--options', metavar="opt:val", nargs='+', type=str,
+        help='Extra options to pass: option name and value, separated by a colon (e.g. --options option1:value1 option2:value2)')
     args = parser.parse_args()
     
     query_proteins = ", ".join(args.UNIPROT_ID)
-    pro_link(query_proteins)
+    options = dict()
+    if args.options:
+        for opt in args.options:
+            opt_val= opt.split(":")
+            if len(opt_val) != 2:
+                print(f"ERROR: Options must be specified as 'option:value'. Got '{opt}'")
+                exit(1)
+            options[opt_val[0]] = opt_val[1]
+    pro_link(query_proteins, **options)
 
 
 if __name__ == '__main__':
