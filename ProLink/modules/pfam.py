06f189c512d86c9d0f28d23afe8a1d5d757bc18a
boneta <boneta@unizar.es>
2023-05-06 13:04:54 +0200

    Fix response when no Pfam domains are found

--- ProLink/modules/pfam.py
+++ ProLink/modules/pfam.py
@@ -7,23 +7,23 @@ from Bio.SeqRecord import SeqRecord
 
 
 def search_hmmer_pfam(seq:str) -> dict:
-    """
-        Use HMMER to search for the Pfam families of a sequence
+    '''
+    Use HMMER to search for the Pfam families of a sequence
 
-        HMMER: Biosequence analysis using profile hidden Markov Models
+    HMMER: Biosequence analysis using profile hidden Markov Models
 
-        Adapted from ProDy: prody.database.pfam @ github.com/ProDy/ProDy
+    Adapted from ProDy: prody.database.pfam @ github.com/ProDy/ProDy
 
-        Parameters
-        ----------
-        seq : str
-            protein sequence to search for
+    Parameters
+    ----------
+    seq : str
+        Protein sequence to search for
 
-        Returns
-        -------
-        dict
-            dictionary with the Pfam families and information of them
-    """
+    Returns
+    -------
+    dict
+        Dictionary with the Pfam families and information of them
+    '''
     parameters = {
         'hmmdb': 'pfam',
         'seq': f">Seq\n{seq}"}
@@ -37,7 +37,7 @@ def search_hmmer_pfam(seq:str) -> dict:
     try:
         tsv = urllib.request.urlopen(result_request).read().decode()
     except:
-        raise ValueError('No matching Pfam domains were found.')
+        raise Exception('No matching Pfam domains were found.')
     lines = tsv.split('\n')
     keys = lines[0].split('\t')
     root = dict()
@@ -67,33 +67,42 @@ def search_hmmer_pfam(seq:str) -> dict:
         matches[pfam_id]['type'] = 'Pfam-A'
     return matches
 
-def fasta_to_dfasta(my_seq_record, fa_to_dfa_input, fa_to_dfa_output):
-  print("Checking Pfam domains")
-  try:
-    my_sequence_domains = search_hmmer_pfam(str(my_seq_record.seq)).keys()
-  except KeyError:
-    my_sequence_domains = search_hmmer_pfam(str(my_seq_record.seq)).keys()
+def fasta_to_dfasta(seq_record:SeqRecord, fasta_input:str, fasta_output:str) -> None:
+    '''
+    Find the Pfam domain of a sequence and compare them with the Pfam domains of the sequences in a fasta file
 
-  d_sequences = []
-  for seq_record in SeqIO.parse(fa_to_dfa_input, "fasta"):
+    Parameters
+    ----------
+    seq_record : SeqRecord
+        Sequence to search
+    fasta_input : str
+        Input fasta file to read
+    fasta_output : str
+        Output fasta file to write
+    '''
+    print("Checking Pfam domains")
     try:
-      subject_sequence_domains = search_hmmer_pfam(str(seq_record.seq)).keys()
-    except KeyError:
-      subject_sequence_domains = "No_domains_found"
-    if my_sequence_domains != subject_sequence_domains:
-      domains = "DD:" + str(subject_sequence_domains).replace("dict_keys", "").replace("([","").replace("])","").replace("'","")
-    else:
-      domains = "SD"
-    rec_d = SeqRecord(
-      Seq(str(seq_record.seq)),
-      id= seq_record.id + seq_record.description + "|" +domains,
-      description = ""
-    )
-    string = rec_d.id
-    new_string = string[:string.find("|_")+1].replace("ref", "") + string[string.find("[")+1:string.find("]")] + string[string.find("]|")+1:]
-    rec_d.id = new_string
-    rec_d.description = ""
-    d_sequences.append(rec_d)
-  SeqIO.write(d_sequences, fa_to_dfa_output, "fasta")
-
-
+        my_sequence_domains = search_hmmer_pfam(str(seq_record.seq)).keys()
+    except:
+        print("No Pfam domains found for the query sequence. Skipping.")
+        return
+    d_sequences = []
+    for seq_record in SeqIO.parse(fasta_input, "fasta"):
+        try:
+            subject_sequence_domains = search_hmmer_pfam(str(seq_record.seq)).keys()
+        except:
+            subject_sequence_domains = "No_domains_found"
+        if my_sequence_domains != subject_sequence_domains:
+            domains = "DD:" + str(subject_sequence_domains).replace("dict_keys", "").replace("([","").replace("])","").replace("'","")
+        else:
+            domains = "SD"
+        rec_d = SeqRecord(
+            Seq(str(seq_record.seq)),
+            id = seq_record.id + seq_record.description + "|" + domains,
+            description = "")
+        string = rec_d.id
+        new_string = string[:string.find("|_")+1].replace("ref", "") + string[string.find("[")+1:string.find("]")] + string[string.find("]|")+1:]
+        rec_d.id = new_string
+        rec_d.description = ""
+        d_sequences.append(rec_d)
+    SeqIO.write(d_sequences, fasta_output, "fasta")
