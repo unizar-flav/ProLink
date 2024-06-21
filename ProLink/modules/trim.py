7e74051cd190b5553523ae20168fddb6d88280cf
boneta <boneta@unizar.es>
2024-06-21 19:52:43 +0200

    Update ClipKIT version dependency

--- ProLink/modules/trim.py
+++ ProLink/modules/trim.py
@@ -2,9 +2,7 @@
 import logging
 from textwrap import dedent
 
-from clipkit.clipkit import execute as clipkit_execute
-from clipkit.helpers import SeqType
-from clipkit.modes import TrimmingMode
+from clipkit.api import clipkit
 
 
 logger = logging.getLogger()
@@ -24,23 +22,18 @@ def trim_align(alignment_fastafile:str, alignment_trim_fastafile:str, mode:str='
         Modes: 'smart-gap', 'gappy', 'kpic', 'kpic-smart-gap', 'kpic-gappy', 'kpi', 'kpi-smart-gap', 'kpi-gappy'
     '''
     logger.info(f"\n-- Trimming alignment with ClipKIT\n")
-    clipkit_stats = clipkit_execute(
-        input_file = alignment_fastafile,
-        input_file_format = 'fasta',
-        output_file = alignment_trim_fastafile,
-        output_file_format = 'fasta',
-        sequence_type = SeqType('aa'),
-        gaps = 0.9,
-        complement = False,
-        mode = TrimmingMode(mode),
-        use_log = False
-        )["stats"]
+    clipkit_stats = clipkit(
+        input_file_path = alignment_fastafile,
+        output_file_path = alignment_trim_fastafile,
+        mode = mode
+        )[1]
     logger.info(dedent(f"""\
         Trimming statistics:
         Alignment file:           {alignment_fastafile}
         Trimmed alignment file:   {alignment_trim_fastafile}
         Trimming mode:            {mode}
-        Number of sites kept:     {clipkit_stats["output_length"]}
-        Number of sites trimmed:  {clipkit_stats["trimmed_length"]}
-        Percentage of alignment trimmed: {clipkit_stats["trimmed_percentage"]:.3f}%
+        Alignment lenght:         {clipkit_stats.alignment_length}
+        Number of sites kept:     {clipkit_stats.output_length}
+        Number of sites trimmed:  {clipkit_stats.trimmed_length}
+        Percentage of alignment trimmed: {clipkit_stats.trimmed_percentage:.3f}%
         """))
